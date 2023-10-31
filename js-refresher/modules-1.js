17c76a22e1da6dab12f9ac6b34a40e1cb05f18f1
HazelDaniel <hazeldaniel856@gmail.com>
2023-10-31 15:37:38 +0100

    re-visiting module: practiced named exports and default exports

--- js-refresher/modules-1.js
+++ js-refresher/modules-1.js
@@ -1,20 +1,47 @@
 //using iife induced namespace
-Module1 =(
-	function () {
-		const list = [];
+// Module1 =(
+// 	function () {
+// 		const list = [];
 
-		const addTwoNumbers = (a, b) => {
-			console.log(a+b);
-		}
+// 		const addTwoNumbers = (a, b) => {
+// 			console.log(a+b);
+// 		}
 
-		const addToList = (a) => {
-			list.push(a);
-		}
+// 		const addToList = (a) => {
+// 			list.push(a);
+// 		}
 
-		return {
-			addTwoNumbers,
-			addToList,
-			list
-		}
-	}
-)();
+// 		return {
+// 			addTwoNumbers,
+// 			addToList,
+// 			list
+// 		}
+// 	}
+// )();
+
+
+// const list = [];
+
+// const addToList = (a) => {
+// 	list.push(a);
+// }
+
+
+// // using named exports
+// export {list, addToList};
+
+
+// const list = [];
+//using default exports
+// export default (a) => {
+// 	list.push(a);
+// 	console.log(list);
+// }
+//
+//mixing default exports with named exports
+export default function (a) {
+	list.push(a);
+}
+const list = [];
+
+export {list};
--- js-refresher/modules-2.js
+++ js-refresher/modules-2.js
@@ -1,4 +1,25 @@
 //importing from an iife induced namespace
-Module1.addToList(2);
-Module1.addToList(3);
-console.log(Module1.list);
+// Module1.addToList(2);
+// Module1.addToList(3);
+// console.log(Module1.list);
+//
+
+//using named imports
+// import {list, addToList} from "./modules-1.js";
+
+// addToList(2);
+// addToList(0);
+// addToList(1);
+// console.log(list);
+
+//using default imports
+// import addToList from "./modules-1.js";
+// addToList(2);
+// addToList(2);
+
+//mixing default imports with named imports
+// import {default as addToList, list} from "./modules-1.js";
+import addToList, {list} from "./modules-1.js";
+
+addToList(23);
+console.log(list);
