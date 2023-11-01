fd3e14c1d63bb9761d67225230e845c42b1930a8
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-01 23:25:46 +0100

    ts-refresher: touched enums and tuples

--- ts-refresher/day-1.js
+++ ts-refresher/day-1.js
@@ -2,6 +2,7 @@
 var str = "0";
 var num = 5;
 var general = 15;
+//type assertion
 num = str;
 var _new;
 _new = general;
@@ -14,14 +15,13 @@ var addToList = function (item1, item2) {
     if (item2)
         list.push(item2);
 };
-var addToStrList = function (item1, item2) {
-    if (!strList.includes(item1))
-        strList.push(item1);
-    if (item2 && item2.match("\w+@\w+.\w+") && !strList.includes(item2))
-        strList.push(item2);
-};
+// const addToStrList = (item1: string, item2?: string): void => {
+//   if (!strList.includes(item1)) strList.push(item1);
+//   if (item2 && item2.match("\w+@\w+.\w+") && !strList.includes(item2))
+//     strList.push(item2);
+// };
 addToList(2, 23);
 console.log(list);
-addToStrList("home :", "victoria");
-addToStrList("home :", "vic@toria.com");
+// addToStrList("home :", "victoria");
+// addToStrList("home :", "vic@toria.com");
 console.log(strList);
--- ts-refresher/day-1.ts
+++ ts-refresher/day-1.ts
@@ -10,23 +10,52 @@ let _new;
 _new = <string>general;
 console.log(num);
 console.log(_new);
-const list: Array<number> = [];
-const strList: Array<string> = [];
+const list:number[] = [];
+const strList:string[] = [];
 
 const addToList = (item1: number, item2?: number): void => {
   list.push(item1);
   if (item2) list.push(item2);
 };
 
-const addToStrList = (item1: string, item2?: string): void => {
-  if (!strList.includes(item1)) strList.push(item1);
-  if (item2 && item2.match("\w+@\w+.\w+") && !strList.includes(item2))
-    strList.push(item2);
-};
+// const addToStrList = (item1: string, item2?: string): void => {
+//   if (!strList.includes(item1)) strList.push(item1);
+//   if (item2 && item2.match("\w+@\w+.\w+") && !strList.includes(item2))
+//     strList.push(item2);
+// };
 
 addToList(2, 23);
 console.log(list);
-addToStrList("home :", "victoria");
-addToStrList("home :", "vic@toria.com");
+// addToStrList("home :", "victoria");
+// addToStrList("home :", "vic@toria.com");
 
 console.log(strList);
+
+//tuples
+
+const someObj: {
+	role: [number, string];
+} = {
+	role: [1, "admin"]
+}
+
+//caught by tsc
+// someObj.role[0] = "2";
+// uncaught by tsc because there is no way for
+// it to know the number of times your function/method is being called
+someObj.role.push(100);
+//works
+someObj.role = [20, "read-only user"];
+//caught by tsc
+// someObj.role = [];
+
+
+// enums
+
+enum Role {ADMIN, READ_ONLY_USER};
+
+const newObj: {
+	role: [number, string];
+} = {
+	role: [Role.ADMIN, "admin"],
+}
